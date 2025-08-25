60cc79127588b861a293f9f3ee5601636d4c21d3
ari-deji <dakmud@gmail.com>
2025-08-25 16:16:54 -0400

    Edits to the analysis.sql file, removing the ..> that were copy pasted from the cmd prompt window

--- analysis.sql
+++ analysis.sql
@@ -16,45 +16,45 @@ SELECT product_name, price FROM products WHERE price>=30000;
 -- First query to return orders by customer
 
 SELECT first_name, last_name, COUNT(order_id)
-   ...> FROM customers
-   ...> JOIN orders ON customers.customer_id = orders.customer_id
-   ...> GROUP BY customers.customer_id;
+   FROM customers
+   JOIN orders ON customers.customer_id = orders.customer_id
+   GROUP BY customers.customer_id;
 
 -- second query to return the total number of orders
 
 SELECT COUNT(order_id) AS total_orders   
-   ...> FROM orders;
+   FROM orders;
 
 -- Joining tables (Q4)
 
 SELECT  orders.order_id, customers.first_name, products.product_name
-   ...> FROM orders 
-   ...> JOIN customers ON orders.customer_id = customers.customer_id           
-   ...> JOIN products ON orders.product_id = products.product_id;
+   FROM orders 
+   JOIN customers ON orders.customer_id = customers.customer_id           
+   JOIN products ON orders.product_id = products.product_id;
 
 
 -- Grouping and Aggregating (Q5)
 
 SELECT first_name, COUNT(order_id)
-   ...> FROM customers 
-   ...> JOIN orders ON customers.customer_id = orders.customer_id
-   ...> GROUP BY customers.customer_id;
+   FROM customers 
+   JOIN orders ON customers.customer_id = orders.customer_id
+   GROUP BY customers.customer_id;
 
 
 -- Calculation and Aliasing
 
 SELECT orders.order_id, products.product_name, orders.quantity * products.price AS total_price
-   ...> FROM orders
-   ...> JOIN products ON orders.product_id = products.product_id;
+   FROM orders
+   JOIN products ON orders.product_id = products.product_id;
 
 
 -- Complex JOIN & Ordering
 
 SELECT first_name, last_name, SUM(quantity *price) AS total_spent
-   ...> FROM customers
-   ...> JOIN orders ON customers.customer_id = orders.customer_id
-   ...> JOIN products ON orders.product_id = products.product_id
-   ...> GROUP BY customers.customer_id
-   ...> ORDER BY total_spent DESC;
+   FROM customers
+   JOIN orders ON customers.customer_id = orders.customer_id
+   JOIN products ON orders.product_id = products.product_id
+   GROUP BY customers.customer_id
+   ORDER BY total_spent DESC;
 
 
